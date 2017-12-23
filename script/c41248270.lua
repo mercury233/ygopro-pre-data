d59bc42db564829d518f47da0aa63d4c2ed1f9b4
mercury233 <me@mercury233.me>
2017-12-23 23:19:15 +0800

    update scripts

---
+++ script/c41248270.lua
@@ -0,0 +1,8 @@
+--トラフィックゴースト
+--Traffic Ghost
+--Script by nekrozar
+function c41248270.initial_effect(c)
+	--link summon
+	c:EnableReviveLimit()
+	aux.AddLinkProcedure(c,nil,3,3)
+end
---
+++ script/c77637979.lua
@@ -0,0 +1,8 @@
+--LANフォリンクス
+--LAN Pholinks
+--Script by nekrozar
+function c77637979.initial_effect(c)
+	--link summon
+	c:EnableReviveLimit()
+	aux.AddLinkProcedure(c,nil,2,2)
+end
